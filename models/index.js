a2bdab2f7ccddc219f69b0707fb52206b3706543
Dmitry <dmitry.balduev@gmail.com>
2021-05-13 18:28:07 -0400

    fixed code for tags-products relations

--- models/index.js
+++ models/index.js
@@ -16,14 +16,19 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: "ProductTag",
+Product.belongsToMany(Tag, 
+  {
+  through: {
+   model: ProductTag
+  },
   foreignKey: "product_id",
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through:"ProductTag",
+  through:{
+    model: ProductTag,
+  },
   foreignKey: "tag_id",
 })
 
