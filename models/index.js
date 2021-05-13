cb4a719d014220bb178b56ffb925b80745eb0d24
Dmitry <dmitry.balduev@gmail.com>
2021-05-12 20:17:15 -0400

    added models and realationships

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: "id",
+})
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongtsToMany(Tag, {
+  through: "ProductTag",
+  foreignKey: "product_id",
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongtsToMany(Product, {
+  through:"ProductTag",
+  foreignKey: "tag_id",
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
